60f6e1fc8bad57d42a1cecdaa7e517dcb7f04041
computermouth <computermouth@crunchbangplusplus.org>
2019-07-22 20:42:36 -0700

    Push up x/y bouncing code

--- src/main.c
+++ src/main.c
@@ -18,14 +18,47 @@ int main( int argc, char * argv[] ) {
 	
 	init_face();
 	
+	int x_direction = 1;
+	int y_direction = 1;
+	
 	// primary loop
 	while(!ww_window_received_quit_event()) {
 		
 		// update events
 		ww_window_update_events();
 		
+		// quit
 		if(keystate.esc == 1){
 			printf("ESC pressed\n");
+			ww_window_send_quit_event();
+		}
+		
+		// set animation
+	    if(keystate.w == 1 && face->active_animation == 0){
+			face->active_animation = 1;
+		} else if(keystate.w == 1){
+			face->active_animation = 0;
+		}
+		
+		// move x
+		face->pad_x = face->pad_x + x_direction;
+		if(face->pad_x > 1000){
+			x_direction = -1;
+		}
+		
+		if(face->pad_x < 0){
+			x_direction = 1;
+		}
+            					                                  	
+		// x_direction = -1
+		
+		// move y
+		face->pad_y = face->pad_y + y_direction;
+		if(face->pad_y > 500){
+			y_direction = -1;
+		}
+		if(face->pad_y < 0){
+			y_direction = 1;
 		}
 		
 		ww_draw_sprite(face);
